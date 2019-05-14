2867414ada8c97f47bfcccc2654d5dae383068bd
max keller <max.keller427@gmail.com>
2019-05-13 22:08:22 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
-
+const express = "express";
+const logger = require("morgan");
+const helmet = require("helmet");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+server.use(express.json());
+server.use(logger("dev"));
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
