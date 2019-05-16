a4ebe101e7dfa631d0c519f86013ced0ecf863aa
max keller <max.keller427@gmail.com>
2019-05-15 19:31:08 -0600

    everything working except for a few small bugs

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const Post = require("./postDb.js");
+const db = require("./postDb.js");
 
 router.get("/", async (req, res) => {
   try {
-    const posts = await Post.get(req.query);
+    const posts = await db.get(req.query);
     res.status(200).json({ posts });
   } catch (err) {
     res.status(500).json({
@@ -15,14 +15,59 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validatePostId, (req, res) => {
+  res.status(201).json(req.post);
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", validatePostId, async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "post removed yo" });
+    } else {
+      res.status(404).json({ message: "the post does not exist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error processing request" });
+  }
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", validatePostId, validatePost, async (req, res) => {
+  try {
+    const post = await db.update(req.params.id, req.body);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "post cant be found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error processing request" });
+  }
+});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+async function validatePostId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const post = await db.getById(id);
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(500).json({ message: "user id invalid" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "the request couldnt be processed" });
+  }
+}
+
+function validatePost(req, res, next) {
+  if (req.body && Object.keys(req.body).length) {
+    next();
+  } else {
+    res.status(500).json({ message: "please include a body" });
+  }
+}
 
 module.exports = router;
